dc70b2a71dbb7e9d6a2c11873c006bd681e6fea4
Tiago Matinho <tiagomartinho97@gmail.com>
2020-11-24 09:16:55 +0000

    Update

--- js/video.js
+++ js/video.js
@@ -1,8 +1,10 @@
 var fps = 20;
 var timmer = 1000/fps;
 
-var frames_counter = 0;
-var frames_max = 150;
+var embrace_hrz_frames_counter = 0;
+var embrace_hrz_frames_max = 150;
+var embrace_vrt_frames_counter = 50;
+var embrace_vrt_frames_max = 150;
 var time_frames_counter = 0;
 var time_frames_max = 52;
 var why_frames_counter = 0;
@@ -14,10 +16,14 @@ $(document).ready(function(){
 
 function nextFrame() {
   // increment frame counter
-  frames_counter++;
+  embrace_hrz_frames_counter++;
   // if needed wrap
-  if(frames_counter == frames_max)
-    frames_counter = 0;
+  if(embrace_hrz_frames_counter == embrace_hrz_frames_max)
+    embrace_hrz_frames_counter = 0;
+
+  embrace_vrt_frames_counter++;
+  if(embrace_vrt_frames_counter == embrace_vrt_frames_max)
+    embrace_vrt_frames_counter = 0;
 
   time_frames_counter++;
   if(time_frames_counter == time_frames_max)
@@ -39,18 +45,18 @@ function nextFrame() {
 
 function embrace() {
   // show next frame
-  $("#embrace-hrz").attr("src","images/embrace-hrz/pic" + frames_counter + ".png");
-  $("#embrace-vrt").attr("src","images/embrace-vrt/pic" + frames_counter + ".png");
+  $("#embrace-hrz").attr("src","images/embrace-hrz/pic" + embrace_hrz_frames_counter + ".png");
+  $("#embrace-vrt").attr("src","images/embrace-vrt/pic" + embrace_vrt_frames_counter + ".png");
 }
 
 function its() {
   // show next frame
-  $("#its").attr("src","images/its/pic" + frames_counter + ".png");
+  $("#its").attr("src","images/its/pic" + embrace_hrz_frames_counter + ".png");
 }
 
 function how() {
   // show next frame
-  $("#how").attr("src","images/how/pic" + frames_counter + ".png");
+  $("#how").attr("src","images/how/pic" + embrace_hrz_frames_counter + ".png");
 }
 
 function time() {
